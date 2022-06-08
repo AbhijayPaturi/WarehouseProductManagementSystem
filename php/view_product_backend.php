0c38bfa2a4395a9b0ee2df6a459d0a322e762e71
AbhijayPaturi <12980677+AbhijayPaturi@users.noreply.github.com>
2022-06-08 08:32:50 -0400

    Update view_product_backend.php
    Removed unnecessary comments + spacing

--- php/view_product_backend.php
+++ php/view_product_backend.php
@@ -1,60 +1,47 @@
 <?php
 	require "../config/config.php";
-
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 	if ( $mysqli->errno ) {
 		echo $mysqli->error;
 		exit();
 	}
 	$mysqli->set_charset('utf8');
-
-	$sql = "SELECT products.id AS products_id, name, quantity, warehouse_section, price, conditions.condition FROM products LEFT JOIN conditions ON products.conditions_id = conditions.id WHERE 1=1";
-
-	// Handle empty/not set input 
+	$sql = "SELECT products.id AS products_id, name, quantity, warehouse_section, price, conditions.condition FROM products LEFT JOIN conditions ON products.conditions_id = conditions.id WHERE 1=1"; 
 	if ( isset($_GET['prodName']) && !empty($_GET['prodName']) ) {
 
 	$sql = $sql . " AND products.name LIKE '%" . $_GET["prodName"] . "%'";
-
 	} else {
 	}
-
 	if ( isset($_GET['manufacturerId']) && !empty($_GET['manufacturerId']) ) {
 	if ($_GET['manufacturerId'] != 0) {
 	    $sql = $sql . " AND products.manufacturers_id = " . $_GET["manufacturerId"];
 	}
 	else {
 	}
-
 	} else {
 	}
-
 	if ( isset($_GET['categoryId']) && !empty($_GET['categoryId']) ) {
 	if ($_GET['categoryId'] != 0) {
 	    $sql = $sql . " AND products.categories_id = " . $_GET["categoryId"];
 	}
 	else {
 	}
-
 	} else { 
 	}
-
 	if ( isset($_GET['warehouseAisle']) && !empty($_GET['warehouseAisle']) ) {
 	if ($_GET['warehouseAisle'] != '0') {
 	    $sql = $sql . " AND products.warehouse_section LIKE '" . $_GET["warehouseAisle"] . "'";
 	}
 	else {
 	}
-
 	} else {
 	}
-
 	if ( isset($_GET['conditionId']) && !empty($_GET['conditionId']) ) {
 		if ($_GET['conditionId'] != 0) {
 		    $sql = $sql . " AND products.conditions_id = " . $_GET["conditionId"];
 		}
 		else {
 		}
-
 		} else {
 	}
 	$sql = $sql . ";";
